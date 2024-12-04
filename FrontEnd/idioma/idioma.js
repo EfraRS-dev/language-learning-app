d3d9f2b66505e268ab1ff6d21aef4cbc8852e1bc
amyepes <amyepes@uninorte.edu.co>
2024-12-04 10:49:54 -0500

    Botón funcional sin enviar a endpoint
    El botón sirve para pasar al dashboard pero tiene desactivo lo de esperar a la respuesta de la base de datos por lo que está listo para implementarle un endpoint y que sea funcional

--- FrontEnd/idioma/idioma.js
+++ FrontEnd/idioma/idioma.js
@@ -1,8 +1,5 @@
+// Función para seleccionar el idioma
 async function chooseIdiom(idiomID) {
-    // const username = document.getElementById('newusername').value;
-    // const email = document.getElementById('newemail').value;
-    // const password = document.getElementById('newpassword').value;
-    const idiomID = idiomID;
     const url = 'http://127.0.0.1:8000/idiom';
 
     try {
@@ -16,14 +13,36 @@ async function chooseIdiom(idiomID) {
 
         const data = await response.json();
 
-        if (response.ok) {
-        } else {
-            alert(data.detail || 'Error al registrarse. Por favor, verifica los datos.');
+        if (!response.ok) {
+            alert(data.detail || 'Error al seleccionar idioma. Por favor, verifica los datos.');
+            return false;
         }
     } catch (error) {
-        console.error('Error during language choosing:', error);
+        console.error('Error durante la selección de idioma:', error);
         alert('Ocurrió un error durante la selección de idioma.');
+        return false;
     }
 
     return true;
 }
+
+// Evento del botón
+document.querySelector('.send-btn').addEventListener('click', async function () {
+    const selectedLanguage = document.querySelector('input[name="radio"]:checked');
+
+    if (selectedLanguage) {
+        const idiomID = selectedLanguage.getAttribute('id2');
+        console.log("Idioma seleccionado:", idiomID);
+
+        //const success = await chooseIdiom(idiomID);
+        success = true;
+
+        if (success) {
+            alert(`Idioma seleccionado: ${selectedLanguage.nextElementSibling.textContent.trim()}`);
+            // Redirige a la nueva página
+            window.location.href = '../dashboard/dashboard.html';
+        }
+    } else {
+        alert("Por favor selecciona un idioma.");
+    }
+});
